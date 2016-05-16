29f64c86f6658434624640c7b98f4d556f64c41e
mcuencaz <m.cuenca@alumnos.upm.es>
2016-05-16 20:25:14 +0200

    Creacion y edicion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 
 /* GET home page. */
@@ -12,6 +13,7 @@ router.get('/', function(req, res, next) {
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('userId', userController.load);  // autoload :userId
 
 // Definicion de rutas de /quizzes
 router.get('/quizzes.:format?', quizController.index);
@@ -26,6 +28,16 @@ router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
+
+
+// Definicion de rutas de cuenta
+router.get('/users',					userController.index);
+router.get('/users/:userId(\\d+)',		userController.show);
+router.get('/users/new',				userController.new);
+router.post('/users',					userController.create);
+router.get('/users/:userId(\\d+)/edit',		userController.edit);
+router.put('/users/:userId(\\d+)',		userController.update);
+router.delete('/users/:userId(\\d+)',		userController.destroy);
 
 
 
